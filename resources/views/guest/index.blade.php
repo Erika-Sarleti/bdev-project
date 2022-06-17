1b23d819e2d6fd718e85f6750295019776ed12b7
ErikaSarleti <erikasarleti00@gmail.com>
2022-06-17 15:10:33 +0200

    boh

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -2,20 +2,20 @@
 @section('pageMain')
     <div class="col-7 container-fluid d-flex flex-wrap justify-content-between">
         @foreach ($devs as $dev)
-    <div class="card mt-5" style="width: 18rem;">
-        <img src="... class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">
-                <span>{{$dev->name}}</span>
-            </h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="{{ route('guest.show', $dev->id)}}" class="btn btn-primary">Vai allo sviluppatore</a>
+        <div class="card mt-5" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">
+                    <span>{{$dev->name}}</span>
+                </h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="{{ route('guest.show', $dev->id)}}" class="btn btn-primary">Vai allo sviluppatore</a>
 
 
-            Ciao a tutti
+                Ciao a tutti
+            </div>
         </div>
-    </div>
-    @endforeach
+        @endforeach
     </div>
     
 @endsection
