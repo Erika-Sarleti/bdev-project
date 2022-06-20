82cda18bfa86859456915eb6826caf8ca2088e85
ALESSANDROUVA <a.uva1988@gmail.com>
2022-06-20 19:35:55 +0200

    Footer complete!

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,41 +1,74 @@
-<footer class="bg-dark">
+<footer>
 
-    <div class="container" style="height:100vh">
-        <div class="row">
-            
-            <div class="col-6 d-flex justify-content-center align-items-center" style="height:100vh">
-                <div class="footer-copyright">
-                    <p class="text-white">Copyright &copy; 2022 - <a href="https://www.linkedin.com/in/giuseppe-cavallaro-a8b8b8b8/">Ciccio</a></p>
+    <div class="bg-dark" style="height:100%">
+        <div class="my-text-color-animated d-flex justify-content-center align-items-center pt-5">
+            <i class="fa-brands fa-connectdevelop fs-1"></i>
+        </div>
+        <div class="container">
+            <div class="row">   
+                <div class="col-6 d-flex justify-content-center align-items-center" style="height:100vh">
+                    <div class="footer-copyright">
+                        <p class="text-white">Copyright &copy; 2022 - <a href="https://www.linkedin.com/in/giuseppe-cavallaro-a8b8b8b8/">Ciccio</a></p>
+                    </div>
+
+                    <div class="text-white">
+                        <p>PRIVACYPOLICY  |  POLICYCOOKIE |  CICCIO - P.I e C.F. 00453310351</p>
+                    </div>
                 </div>
 
-                <div class="text-white">
-                    <p>PRIVACYPOLICY  |  POLICYCOOKIE |  CICCIO - P.I e C.F. 00453310351</p>
-                </div>
+                <div class="col-6 d-flex justify-content-center align-items-center" style="height:100vh">
+                    <div class="text-white">
+                        <ul class="list-group-flush" >
+                            <li class="my-text-color mt-5">
+                                <a href="#" class="text-white">
+                                    Registrati
+                                </a>
+                            </li>
+                            <li class="my-text-color mt-5">
+                                <a href="#" class="text-white">
+                                Cerchi uno sviluppatore?
+                                </a>
+                            </li>
+                            <li class="my-text-color mt-5">
+                                <a href="#" class="text-white">
+                                Lavora con noi
+                                </a>    
+                            </li>
+                            <li class="my-text-color mt-5">
+                                <a href="#" class="text-white">
+                                Vuoi darci dei consigli?
+                                </a>    
+                            </li>
+                        </ul>
+                    </div>
+                
             </div>
-
-            <div class="col-6 d-flex justify-content-center align-items-center" style="height:100vh">
-                <div class="text-white">
-                    <ul class="list-group-flush" >
-                        <li class="my-text-color mt-5">Registrati</li>
-                        <li class="my-text-color mt-5">Cerchi uno sviluppatore?</li>
-                        <li class="my-text-color mt-5">Lavora con noi</li>
-                        <li class="my-text-color mt-5">Vuoi darci dei consigli?</li>
-                    </ul>
-                </div>
-            
         </div>
     </div>
-    <div>
-        <ul>
+    <div class="bg-dark">
+        <ul class="d-flex justify-content-center align-items-center mb-0 pb-3">
             <li class="my-text-color">
+                <a href="wwww.facebook.it" class="text-white">
                 <i class="fa-brands fa-facebook-f"></i>
+                </a>
+            </li>
+            <li class="my-text-color mx-5">
+                <a href="wwww.instagram.it"  class="text-white">
+                <i class="fa-brands fa-instagram"></i>
+                </a>
             </li>
             <li class="my-text-color">
-                <i class="fa-brands fa-instagram"></i>
+                <a href="wwww.youtube.it" class="text-white">
+                <i class="fa-brands fa-youtube"></i>
             </li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li class="my-text-color mx-5">
+                <a href="wwww.twitter.it" class="text-white">
+                <i class="fa-brands fa-twitter"></i>
+            </li>
+            <li class="my-text-color">
+                <a href="wwww.linkedin.it" class="text-white">
+                <i class="fa-brands fa-linkedin-in"></i>
+            </li>
         </ul>
     </div>
 
