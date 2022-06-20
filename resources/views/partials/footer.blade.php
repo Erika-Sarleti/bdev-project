cb836d0f5ec145cee51d466059506c9188cb6dad
ALESSANDROUVA <a.uva1988@gmail.com>
2022-06-20 18:32:38 +0200

    Footer style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,12 +1,42 @@
-<footer>
-    <div class="bg-dark">
+<footer class="bg-dark">
+
+    <div class="container" style="height:100vh">
         <div class="row">
-            <div class="col-md-12">
+            
+            <div class="col-6 d-flex justify-content-center align-items-center" style="height:100vh">
                 <div class="footer-copyright">
-                    <p>Copyright &copy; 2020 - <a href="https://www.linkedin.com/in/giuseppe-cavallaro-a8b8b8b8/">Giuseppe Cavallaro</a></p>
+                    <p class="text-white">Copyright &copy; 2022 - <a href="https://www.linkedin.com/in/giuseppe-cavallaro-a8b8b8b8/">Ciccio</a></p>
+                </div>
+
+                <div class="text-white">
+                    <p>PRIVACYPOLICY  |  POLICYCOOKIE |  CICCIO - P.I e C.F. 00453310351</p>
                 </div>
             </div>
+
+            <div class="col-6 d-flex justify-content-center align-items-center" style="height:100vh">
+                <div class="text-white">
+                    <ul class="list-group-flush" >
+                        <li class="my-text-color mt-5">Registrati</li>
+                        <li class="my-text-color mt-5">Cerchi uno sviluppatore?</li>
+                        <li class="my-text-color mt-5">Lavora con noi</li>
+                        <li class="my-text-color mt-5">Vuoi darci dei consigli?</li>
+                    </ul>
+                </div>
+            
         </div>
+    </div>
+    <div>
+        <ul>
+            <li class="my-text-color">
+                <i class="fa-brands fa-facebook-f"></i>
+            </li>
+            <li class="my-text-color">
+                <i class="fa-brands fa-instagram"></i>
+            </li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
     </div>
 
 </footer>
