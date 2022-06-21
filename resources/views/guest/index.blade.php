3b6e8039961b28b7af9eb795c1c103591153e134
ALESSANDROUVA <a.uva1988@gmail.com>
2022-06-21 19:12:24 +0200

    Front start

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -1,21 +1,99 @@
 @extends('layouts.base')
+
+
 @section('pageMain')
-    <div class="col-7 container-fluid d-flex flex-wrap justify-content-between">
-        @foreach ($devs as $dev)
-        <div class="card mt-5" style="width: 18rem;">
-            <img src="{{ $dev->userinfo->image }}" class="card-img-top" alt="{{$dev->name}}">
-            <div class="card-body">
-                <h5 class="card-title">
-                    <span>{{$dev->name}}</span>
-                </h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="{{ route('guest.show', $dev->id)}}" class="btn btn-primary">Vai allo sviluppatore</a>
-
-
-                Ciao a tuttii
+    
+    <div class="container-index">
+        <section class="container-section" id="section-start">
+            <div class="center-section">
+                
+                <h4 class="my-text-color">Inizia qui la ricerca dello sviluppatore su misura per il tuo business:</h4>
+                <a href="#" class="btn-neon">Vai alla ricerca</a>
+                
             </div>
-        </div>
-        @endforeach
+                <div class="container-buttons">
+                    <a href="#section-how" class="btn-neon">
+                        v
+                    </a>
+                </div>
+        </section>
+
+        {{-- sezione come usare il sito --}}
+        <section class="container-section" id="section-how">
+
+            <div class="center-section-how">
+                <h3 class="my-text-white">Trovare lo sviluppatore adatto a te 
+                    non è mai stato così semplice</h3>
+                    <div class="d-flex justify-content-evenly align-items-center my-3">
+                        <div class="my-card" style="width: 200px; height:400px">
+                            <img src="https://picsum.photos/200" alt="" class="my-img">
+                            <small class="my-p my-text-white">Ricerca tra millemila sviluppatori iscritti alla nostra piattaforma</small>
+                        </div>
+                        <div class="my-card" style="width: 200px; height:400px">
+                            <img src="https://picsum.photos/200" alt="" class="my-img">
+                            <small class="my-p my-text-white">Mandagli un messaggio. Come su Tinder!</small>
+                        </div>
+                        <div class="my-card" style="width: 200px; height:400px">
+                            <img src="https://picsum.photos/200" alt="" class="my-img">
+                            <small class="my-p my-text-white">Il tuo sito sarà pronto in brevissimo tempo! Perché tu vali. ci metto altro testo perché così simulo un testo lungo, va bene?</small>
+                        </div>
+                    </div>
+                
+            </div>
+
+            <div class="container-buttons">
+                <a href="#section-start" class="btn-neon">
+                    s
+                </a>
+                <a href="#section-evidence" class="btn-neon">
+                    v
+                </a>
+            </div>
+        </section>
+
+        {{-- sezione profili in evidenza --}}
+        <section class="container-section" id="section-evidence">
+            <div class="center-section-evidence">
+                <h3 class="my-text-color">
+                    Scopri la nostra rete di migliori sviluppatori in Italia:
+                    <div class="d-flex overlay">
+                        @foreach ($devs as $dev)
+                            <div class="d-flex flex-column justify-content-center align-items-center">
+                                <a href="{{ route('guest.show', $dev->id) }}" class="my-text-color">
+                                    {{ $dev->name }}
+                                    <img src="{{ $dev->userinfo->image }}" class="my-img-round">
+                                </a>
+                            </div>
+                        @endforeach
+                    </div>
+                </h3>
+            </div>
+
+            <div class="container-buttons">
+                <a href="#section-how" class="btn-neon">
+                    s
+                </a>
+                <a href="#section-say" class="btn-neon">
+                    v
+                </a>
+            </div>
+        </section>
+
+        {{-- sezione dicono di noi --}}
+        <section class="container-section" id="section-say">
+            <div class="center-section">
+                ciao, sono la sezione dicono di noi
+            </div>
+
+            <div class="container-buttons">
+                <a href="#section-evidence" class="btn-neon">
+                    s
+                </a>
+                <a href="#footer" class="btn-neon">
+                    v
+                </a>
+            </div>
+        </section>
     </div>
     
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,31 +1,34 @@
-<header class="bg-dark">
-    <nav class="navbar navbar-expand-lg">
+<header>
+    <nav class="navbar navbar-expand-lg nav-sticky">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
+          <a class="navbar-brand my-text-color d-flex align-items-center" href="{{ route('guest.index')}}">
+            <i class="fa-brands fa-connectdevelop fs-1"></i>
+            <span>dev</span>
+          </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse no-growth" id="navbarNav">
             <ul class="navbar-nav">
               @auth
-                <a class="navbar-brand" href="{{route('admin.devs.edit',  Auth::user()->id )}}">Modifica il profilo</a>
+                <a class="navbar-brand my-text-color" href="{{route('admin.devs.edit',  Auth::user()->id )}}">Modifica il profilo</a>
                 
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="{{route('admin.devs.index')}}">Esplora</a>
+                  <a class="nav-link active my-text-color" aria-current="page" href="{{route('admin.devs.index')}}">Esplora</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#"> Ciao {{ Auth::user()->name }}</a>
+                  <a class="nav-link my-text-color" href="#"> Ciao {{ Auth::user()->name }}</a>
                 </li>
                 @else
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="{{route('guest.index')}}">Esplora</a>
+                  <a class="nav-link active my-text-color" aria-current="page" href="{{route('guest.index')}}">Esplora</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="{{ route('login') }}">Login</a>
+                  <a class="nav-link my-text-color" href="{{ route('login') }}">Login</a>
                 </li>
                 @if (Route::has('register'))
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('register') }}">Register</a>
+                    <a class="nav-link my-text-color" href="{{ route('register') }}">Register</a>
                 </li>
                 @endif
                 
@@ -33,7 +36,7 @@
               
               
               <li class="nav-item">
-                <a class="nav-link" href="#">Help </a>
+                <a class="nav-link my-text-color" href="#">Help </a>
               </li>
             </ul>
           </div>
