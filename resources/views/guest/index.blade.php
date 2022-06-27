49cfe88ce4590f9120c1f4402807dcfc85758f44
ALESSANDROUVA <a.uva1988@gmail.com>
2022-06-28 01:52:27 +0200

    ultimo commit

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -4,46 +4,49 @@
     <div id="app">
 
 
-        <div class="col-7 container-fluid d-flex flex-wrap justify-content-between mt-5">
+        <div class="d-flex flex-column flex-wrap justify-content-between align-items-center mt-5">
 
-                <div class="filters d-flex flex-column mt-5 mb-5">
+            {{-- <div class="filters d-flex flex-column mt-5 mb-5"> --}}
 
-                <label for="role"> FILTRA PER RUOLO </label>
-                <select v-model="role" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <option value="back end">back end</option>
-                    <option value="front end">front end</option>
-                    <option value="full stack">full stack</option>
-                    <option value="data analyst">data analyst</option>
-                </select>
-
-                <button @click="search" class="btn btn-outline-success mb-5 mt-2" type="submit">SEARCH</button>
-
-                
-                <label for="nreview"> NUMERO RECENSIONI </label>
-                <input v-model="nreview" name="nreview" type="number">
-
-                <label for="mediarating"> MEDIA VOTI </label>
-                <input v-model="mediarating" name="mediarating" type="number" min="0" max="5">
+                <div class="col-6 mt-5 d-flex flex-column ">
+                    <label for="role" class="align-self-center"> FILTRA PER RUOLO </label>
+                    <select v-model="role" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <option value="back end">back end</option>
+                        <option value="front end">front end</option>
+                        <option value="full stack">full stack</option>
+                        <option value="data analyst">data analyst</option>
+                    </select>
+    
+                    <button @click="search" class="btn btn-outline-success mb-5 mt-2 px-5" type="submit">SEARCH</button>
+                </div>
 
 
-                <button class="btn btn-outline-success mt-2" @click="filter">APPLY FILTERS</button>
+                <div class="col-6 d-flex flex-column"> 
+                    <label for="nreview" class="align-self-center"> NUMERO RECENSIONI </label>
+                    <input v-model="nreview" name="nreview" type="number">
+                    <label for="mediarating" class="align-self-center"> MEDIA VOTI </label>
+                    <input v-model="mediarating" name="mediarating" type="number" min="0" max="5">
+                    <button class="btn btn-outline-success mt-2" @click="filter">APPLY FILTERS</button>
+                </div>
 
-            </div>
+            {{-- </div> --}}
+        </div>
+            <div class="col-10 container-fluid d-flex flex-wrap justify-content-between my-5">
             
             <div v-for="dev in devs" class="card mt-5" style="width: 18rem;">
                
-                <img :src="dev.image" class="card-img-top" alt="devname">
-                <div class="card-body">
+                <img v-If="dev.image === null" src="https://picsum.photos/300/200" class="card-img-top" :alt="dev.name">
+                <img v-else  :src="dev.image" class="card-img-top" :alt="dev.name">
+                <div class="card-body d-flex flex-column gap-3">
                     <h5 class="card-title">
                         <span>@{{dev.user.name}}</span>
-
                     </h5>
+                    <small>@{{dev.role}}</small>
                     <p class="card-text">@{{dev.description}}</p>
-                    <small> @{{dev.media}}</small>
-                    <small> @{{dev.nreview}}</small>
+                    <small>Media Voti: @{{dev.media}}</small>
+                    <small>Recensioni: @{{dev.nreview}}</small>
 
                     <a :href="linkShow + dev.user.id" class="btn btn-primary">Vai allo sviluppatore</a>
-    
     
                 </div>
             </div>
@@ -51,24 +54,3 @@
     </div>
 
 @endsection
-    
-
-
-
-
-
-{{-- @foreach ($devs as $dev)
-<div class="card mt-5" style="width: 18rem;">
-    <img src="{{ $dev->userinfo->image }}" class="card-img-top" alt="{{$dev->name}}">
-    <div class="card-body">
-        <h5 class="card-title">
-            <span>{{$dev->name}}</span>
-        </h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="{{ route('guest.show', $dev->id)}}" class="btn btn-primary">Vai allo sviluppatore</a>
-
-
-    </div>
-</div>
-@endforeach --}}
-
