b76fe31d368e914a180e877d4bac931e845b6140
ALESSANDROUVA <a.uva1988@gmail.com>
2022-06-27 09:59:08 +0200

    push first merge

--- resources/views/guest/show.blade.php
+++ resources/views/guest/show.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.base')
 @section('pageTitle', 'ShowPage')
 @section('pageMain')
-  <div class="d-flex flex-column ">
-    <div class="card" style="width: 18rem; height: 100vh">
-        <img src="{{ $dev->userinfo->image }}" class="card-img-top" alt="image {{ $dev->name }}">
+
+ {{-- SEZIONE PROFILO --}}
+<section class="container">
+  <div class="d-flex flex-column my-container-show">
+    <div class="card">
+      <img src="{{ $dev->userinfo->image }}" class="my-image" alt="image {{ $dev->name }}">
         <div class="card-body">
           <h5 class="card-title">{{ $dev->name }}</h5>
           <p class="card-text">{{$dev->description}}</p>
@@ -22,11 +25,23 @@
           <a href="#form-message" class="btn btn-primary">Invia un messaggio</a>
         </div>
     </div >
+  </div>
+  </section>
+
+
+  {{-- SEZIONE RECENSIONI --}}
+  <section class="container d-flex justify-content-center align-items-center">
     @php
       $id_dev=$dev->id;
     @endphp
 
     <a href="{{route('guest.create', ['id' => $id_dev])}}">Scrivi una Recensione</a>
+
+  </section>
+
+
+  {{-- SEZIONE MESSAGGI --}}
+  <section class="container">
     
 
     <form action="{{ route("message.store")}}" method="POST" class="mt-3" style="height: 100vh" id="form-message">
@@ -46,4 +61,5 @@
       <button type="submit" class="btn btn-primary">Invia</button>
     </form>
   </div>
+  </section>
 @endsection
